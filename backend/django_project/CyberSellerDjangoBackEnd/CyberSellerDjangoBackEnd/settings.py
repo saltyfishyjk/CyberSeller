30215c8a15455b15a2cde512bc35b85538eaa4e0
YJK <yjk_official@foxmail.com>
2022-10-25 15:22:25 +0800

    feat : add class in new app

--- backend/django_project/CyberSellerDjangoBackEnd/CyberSellerDjangoBackEnd/settings.py
+++ backend/django_project/CyberSellerDjangoBackEnd/CyberSellerDjangoBackEnd/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'CyberSellerBackend.apps.CybersellerbackendConfig',
+    'backendapp.apps.BackendappConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
