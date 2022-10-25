ee87a8da9d425406812846c7d47b839608520604
YJK <yjk_official@foxmail.com>
2022-10-25 12:55:05 +0800

    fix: fix settings

--- backend/django_project/CyberSellerDjangoBackEnd/CyberSellerDjangoBackEnd/settings.py
+++ backend/django_project/CyberSellerDjangoBackEnd/CyberSellerDjangoBackEnd/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Cybersellerbackend.apps.CybersellerbackendConfig',
+    'CyberSellerBackend.apps.CybersellerbackendConfig',
 ]
 
 MIDDLEWARE = [
