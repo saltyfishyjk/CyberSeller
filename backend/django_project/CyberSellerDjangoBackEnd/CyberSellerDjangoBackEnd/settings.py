1b4cee78139ece261e17da896313cd2117d0c9ca
YJK <yjk_official@foxmail.com>
2022-10-25 15:12:31 +0800

    fix: remove app setting

--- backend/django_project/CyberSellerDjangoBackEnd/CyberSellerDjangoBackEnd/settings.py
+++ backend/django_project/CyberSellerDjangoBackEnd/CyberSellerDjangoBackEnd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import CyberSellerBackend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'CyberSellerBackend.apps.CybersellerbackendConfig',
+    # 'CyberSellerBackend.apps.CybersellerbackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
