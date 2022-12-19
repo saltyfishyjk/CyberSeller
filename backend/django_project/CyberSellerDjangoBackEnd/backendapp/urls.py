856376fd996a69739a1af7bec1738b91e44d0f53
YJK <yjk_official@foxmail.com>
2022-12-19 23:53:51 +0800

    feat: 尝试添加addGoods并检查表单是否可以正常工作

--- backend/django_project/CyberSellerDjangoBackEnd/backendapp/urls.py
+++ backend/django_project/CyberSellerDjangoBackEnd/backendapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path(r'signup', views.signup, name='signup'),
 	path(r'login', views.login, name='login'),
+	path(r'addGoods', views.testAddGoods, name='addGoods'),
 ]
