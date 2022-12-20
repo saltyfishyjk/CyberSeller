038b7bbab479ec7e92a6686bfa4a87fd90588f08
YJK <yjk_official@foxmail.com>
2022-12-20 10:17:32 +0800

    feat: 尝试添加addGoods并检查表单是否可以正常工作

--- backend/django_project/CyberSellerDjangoBackEnd/backendapp/urls.py
+++ backend/django_project/CyberSellerDjangoBackEnd/backendapp/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from django.views.static import serve
+from CyberSellerDjangoBackEnd.settings import IMG_UPLOAD
 
 urlpatterns = [
 	path('', views.index, name='index'),
 	path(r'signup', views.signup, name='signup'),
 	path(r'login', views.login, name='login'),
 	path(r'addGoods', views.addGoods, name='addGoods'),
+	re_path(r'^img/(?P<path>.*)$', serve, {
+		'document_root': IMG_UPLOAD
+	})
 ]
