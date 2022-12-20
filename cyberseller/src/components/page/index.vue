51eb72441f04e153190110792abaab5d53560c17
iszry <iszry@foxmail.com>
2022-12-20 23:23:10 +0800

    12-20-frontEnd-editGoodsInfo

--- cyberseller/src/components/page/index.vue
+++ cyberseller/src/components/page/index.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-
-
     <el-carousel :interval="4000" type="card" height="200px" style="background: #F2F8FE">
-      <el-carousel-item v-for="item in imageList" :key="item.address">
-        <img :src="item.address">
+      <el-carousel-item v-for="item in imageList" :key="item">
+        <img :src="item">
       </el-carousel-item>
     </el-carousel>
     <br>
@@ -19,12 +17,9 @@
         </el-input>
         <p style="color: #BFBFBF;">
           <span>热门搜索：</span>
-          <a href="">华为P40</a>
-          <a href="">海尔空调</a>
-          <a href="">海尔空调</a>
-          <a href="">海尔空调</a>
-          <a href="">海尔空调</a>
-          <a href="">惠普打印机</a>
+          <a href="">月饼</a>
+          <a href="">狐狸</a>
+          <a href="">课表</a>
         </p>
 
       </div>
@@ -44,14 +39,13 @@
                       <el-button type="success" style="margin-left:25%;margin-top: 70%;" size="medium" @click="goGoodsDesc(item.id)">查看详情</el-button>
                       <el-button type="warning" icon="el-icon-star-off" circle size="medium"></el-button>
                     </div>
-                    <img :src="item.img" style="height: 100%;width: 100%">
-                    <div class="wrap"><span class="ribbon6" style="color:#000;"><span style="color: #F2F8FE">双十一狂欢{{item.id}}号</span></span>
-                  </div>
+                    <img :src="item.picture" style="height: 100%;width: 100%">
+                    
                 </div>
                 <div style="text-align:center">
                   <span>{{item.name}}</span>
-                  <span>&yen;{{item.prize}}</span>
-                  <span>{{item.desc}}</span>
+                  <span>&yen;{{item.price}}</span>
+                  <span>{{item.seller_name}}</span>
                 </div>
               </div>
         </div>
@@ -73,62 +67,61 @@
 </template>
 
 <script>
-  export default {
-      name: "index",
-      data() {
-        return {
-          ishow: false,
-          current: 0,
-          selectd: '',
-          currentDate: new Date(),
-          imageList:[
-            {id: 1, title: "test1", address: "http://43.143.179.158:8080/img/avatar-%E6%8A%97%E5%8E%9F.jpg"},
-            {id:2,title:"test2", address:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2013319254,3446577563&fm=26&gp=0.jpg"},
-            {id:3,title:"test3", address:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2013319254,3446577563&fm=26&gp=0.jpg"},
-            {id:4,title:"test4", address:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2013319254,3446577563&fm=26&gp=0.jpg"},
-            {id:5,title:"test5", address:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2013319254,3446577563&fm=26&gp=0.jpg"},
-            {id:6,title:"test6", address:"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2013319254,3446577563&fm=26&gp=0.jpg"},
-          ],
-          goodsList:[
-            {id:1,name:"华为 HUAWEI Mate 40 Pro", img:"https://img13.360buyimg.com/n1/jfs/t1/127138/40/16031/89209/5f918b53E19746496/f0f235be5150056f.jpg",prize:"5999.00",desc:"华为官方旗舰店"},
-            {id:2,name:"荣耀Play4 Pro", img:"https://img13.360buyimg.com/n1/s450x450_jfs/t1/136135/36/11503/129859/5f769f36E0a120157/a7bddadf2b8ae6d8.jpg",prize:"2799.00 ",desc:"华为官方旗舰店"},
-            {id:3,name:"联想（Lenovo）YOGA 14s", img:"https://img10.360buyimg.com/n1/jfs/t1/122621/1/15816/121959/5f8ee259E2d2178c6/017ac237a18d2346.jpg",prize:"5766.00",desc:"联想官方旗舰店"},
-            {id:4,name:"Apple iPhone 11   128GB", img:"https://img12.360buyimg.com/n1/s450x450_jfs/t1/79668/22/9987/159271/5d780915Ebf9bf3f4/6a1b2703a9ed8737.jpg",prize:"5899.00",desc:"苹果官方旗舰店"},
-            {id:5,name:"小米Redmi红米k30 至尊纪念版", img:"https://img10.360buyimg.com/n1/s450x450_jfs/t1/138769/33/11422/181706/5f8fabbfE9d52ee11/47b690dbf2d4b147.jpg",prize:"2118.00",desc:"小米旗舰店"},
-            {id:6,name:"小米电视4A 70英寸", img:"https://img11.360buyimg.com/n1/jfs/t1/130034/39/4685/156952/5f119f0fEd0d7d957/1eabe76b0f15b0d8.jpg",prize:"3499.00",desc:"小米旗舰店"},
-            {id:7,name:"小米手环5 NFC版", img:"https://img11.360buyimg.com/n1/s450x450_jfs/t1/142719/12/444/104550/5ee1988cEd952bf51/74860b136a001168.jpg",prize:"229.00",desc:"小米旗舰店"},
-            {id:8,name:"赛睿 西伯利亚 200", img:"https://img11.360buyimg.com/n1/s450x450_jfs/t1/111775/32/11694/132419/5f029811Ed7c79c8b/77d6e3870531b71b.jpg",prize:"319.00",desc:"赛睿旗舰店"},
-            {id:9,name:"雷蛇 Razer 萨诺狼蛛专业版", img:"https://img14.360buyimg.com/n1/s450x450_jfs/t1/113321/24/6567/313554/5ebb576cE79694dd8/d2012f7ebf9a8ec1.jpg",prize:"269.00",desc:"雷蛇官方旗舰店"},
-            {id:10,name:"佳能 EOS M50 微单相机", img:"https://img12.360buyimg.com/n1/s450x450_jfs/t19423/211/1066657577/92794/338e18b0/5abb0640Ndc7d364d.jpg",prize:"4799.00",desc:"佳能官方旗舰店"},
-            {id:11,name:"飞利浦猛腾 31.5英寸 2K曲面", img:"https://img14.360buyimg.com/n1/jfs/t1/143727/10/11420/254435/5f8f9e8bE1551aeda/3febf2c77d016b3e.jpg",prize:"1798.00",desc:"飞利浦显示器旗舰店"},
-            {id:12,name:"TP-LINK 5G 无线路由器", img:"https://img11.360buyimg.com/n1/s450x450_jfs/t1/114981/37/18201/64680/5f6af41bE4c1a5dad/53e06408e93a65eb.jpg",prize:"99.00",desc:"TP-LINK旗舰店"},
-            {id:13,name:"三星 27英寸 1800R曲面", img:"https://img10.360buyimg.com/n1/s450x450_jfs/t1/109252/8/3748/148356/5e1549c6Eb24b13ae/28e893cf441b6fc1.jpg",prize:"999.00",desc:"三星官方旗舰店"},
-
-          ]
-        };
-      },
-      methods:{
-        //前往商品详情页
-        goGoodsDesc(goods) {
-          this.$router.push({
-            path:'/goodsDesc',
-            query:{
-              goods:goods
-            }
-          });
-        },
-        //显示操作项
-        showDialog(index, item) {
-          this.ishow = true;
-          this.current = index;
-        },
-        //隐藏蒙层
-        hideDialog(index, item) {
-          this.ishow = false;
-          this.current = null;
+import { postForm } from '../../api';
+export default {
+  name: "index",
+  data() {
+    return {
+      ishow: false,
+      current: 0,
+      selectd: '',
+      currentDate: new Date(),
+      imageList:null,
+      goodsList:null
+    };
+  },
+  methods:{
+    //前往商品详情页
+    goGoodsDesc(goods) {
+      this.$router.push({
+        path:'/goodsDesc',
+        query:{
+          goods:goods
         }
-      }
+      });
+    },
+    //显示操作项
+    showDialog(index, item) {
+      this.ishow = true;
+      this.current = index;
+    },
+    //隐藏蒙层
+    hideDialog(index, item) {
+      this.ishow = false;
+      this.current = null;
     }
+  },
+  beforeCreate() {
+    console.log('Call Vue beforeCreate');
+    let fd = new FormData()
+    fd.append('user_id', localStorage.getItem('userId'))
+    console.log('user_id' + localStorage.getItem('userId'))
+    postForm(`http://43.143.179.158:8080/mainRecommendGoods`, fd).then(res => {
+      console.log(res)
+      this.goodsList = res.goods
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    postForm(`http://43.143.179.158:8080/getSixPictures`, fd).then(res => {
+      console.log(res)
+      this.imageList = res.pictures
+    })
+      .catch(function (error) {
+        console.log(error);
+    });
+  }
+}
 </script>
 
 <style scoped>
--- cyberseller/src/components/page/sellGoods.vue
+++ cyberseller/src/components/page/sellGoods.vue
@@ -1,49 +1,94 @@
 <template>
-    <div style="text-align: center;width: 20%;margin-left: 40%;margin-top: 30px;">
+    <div class="seller_page">
+    <div class="new_goods" style="position: absolute;width: 500px; height: 600px;border-radius: 14px;">
 
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px">
-        <el-form-item label="商品名" prop="name">
+        <el-form-item label="商品名" prop="name" >
             <el-input v-model="ruleForm.name" style="width: 300px"></el-input>
         </el-form-item>
         <el-form-item label="价格" prop="price">
             <el-input v-model="ruleForm.price" style="width: 300px"></el-input>
         </el-form-item>
-        <el-form-item label="商品图片">
+        <el-form-item label="商品图片" prop="image">
             <input accept=".png, .jpg" type="file" ref="file" @change="getImageFile" />
+        </el-form-item>
+        <el-form-item label="制造商" prop="maker">
+            <el-input v-model="ruleForm.maker" clearable style="width: 300px"></el-input>
+        </el-form-item>
+        <el-form-item label="生产日期" prop="date">
+            <el-input v-model="ruleForm.date" style="width: 300px"></el-input>
+        </el-form-item>
+        <el-form-item label="保质期" prop="shelf_life">
+            <el-input v-model="ruleForm.shelf_life" style="width: 300px"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" prop="description">
+            <el-input v-model="ruleForm.description" style="width: 300px"></el-input>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="submitForm('ruleForm')">发布商品</el-button>
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
+            <el-button @click="resetForm()">重置</el-button>
         </el-form-item>
     </el-form>
         <br>
+        
+    </div>
+        <div class="title" style="width: 65%;margin-left: 30%;" v-if="getSellData()">
+            <h3>售卖中</h3>
+            <el-table 
+                :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+                 style="width: 100%;">
+        
+                <el-table-column label="商品" prop="picture" width="110px" align="center">
+                    <template slot-scope="scope">
+                        <el-image style="width: 100px; height: 100px;" :src="scope.row.picture" />
+                    </template>
+                </el-table-column>
+                <el-table-column label="商品名" prop="name" align="center"></el-table-column>
+                <el-table-column label="状态" prop="prize" width="110px" align="center">
+                    <template slot-scope="scope">
+                        <el-tag :type="goodsStatus(scope.row.status)">{{scope.row.repo==0?"售罄":"在售"}}</el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column label="单价" prop="prize" width="110px" align="center">
+                    <template slot-scope="scope">
+                        <span>&yen;</span>{{scope.row.price}}
+                    </template>
+                </el-table-column>
+                <el-table-column label="库存" prop="repo" width="140px" align="center">
+                    <template slot-scope="scope">
+                        <el-input-number size="mini" v-model="scope.row.repo" value="repo_edit" @change="handleEdit(scope.row)"></el-input-number>
+                    </template>
+                </el-table-column>
+                <el-table-column align="right">
+                    <template slot-scope="scope">
+                        <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">下架商品</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 
 <script>
-import { postForm,post } from "@/api";
+import { postForm } from "@/api";
 export default {
     name: "goodsInfo",
     data() {
         return {
+            tableData: null,
+            search: '',
+            //选中列表
+            multipleSelection: [],
+            chooseList: [],
             editable: true,
-            imageUrl: '',
-            form: {
-                name: '',
-                tel: '',
-                email: '',
-                img: '',
-                price: '',
-                maker: '',
-                picture: '',
-                description: '',
-                date: '',
-                shelf_life: ''
-            },
             ruleForm: {
                 name: '',
                 image: null,
-                price: ''
+                price: '',
+                maker: '',
+                description: '',
+                date: '',
+                shelf_life: ''
             },
             rules: {
                 name: [
@@ -55,17 +100,45 @@ export default {
                 price: [
                     { required: true, message: '请输入商品价格', trigger: 'blur' },
                 ],
+                maker: [
+                    { required: false, message: '请输入制造商', trigger: 'blur' },
+                ],
+                description: [
+                    { required: false, message: '请输入商品描述信息', trigger: 'blur' },
+                ], 
+                date: [
+                    { required: false, message: '请输入生产日期', trigger: 'blur' },
+                ],
+                shelf_life: [
+                    { required: false, message: '请输入保质期', trigger: 'blur' },
+                ],
             }
         }
     },
     methods: {
         submitForm(formName) {
             let fd = new FormData()
-            fd.append('name', formName.name)
-            fd.append('price', formName.price)
+            fd.append('name', this.ruleForm.name)
+            fd.append('price', this.ruleForm.price)
             fd.append('picture', this.ruleForm.image)
             fd.append('seller_id', localStorage.getItem('userId'))
-            console.log(typeof this.ruleForm.image)
+            if (this.ruleForm.maker != '') {
+                fd.append('maker', this.ruleForm.maker)
+            }
+            if (this.ruleForm.description != '') {
+                fd.append('description', this.ruleForm.description)
+            }
+            if (this.ruleForm.date != '') {
+                fd.append('date', this.ruleForm.date)
+            }
+            if (this.ruleForm.shelf_life != '') {
+                fd.append('shelf_life', this.ruleForm.shelf_life)
+            }
+            console.log(typeof this.ruleForm.name)
+            console.log(typeof this.ruleForm.price)
+            console.log(this.ruleForm.price)
+            console.log(localStorage.getItem('userId'))
+            
             postForm(`http://43.143.179.158:8080/addGoods`, fd).then(res => {
                 console.log(res)
             })
@@ -85,9 +158,70 @@ export default {
         console.log(typeof this.ruleForm.image)
         },
         resetForm(formName) {
-            this.$refs[formName].resetFields();
+            this.$refs.ruleForm.resetFields()
             this.ruleForm.image = null;
         },
+        canSelect(row) {
+            if (row.status == 1)
+                return false;
+            else return true;
+        },
+        handleEdit(row) {
+            let repo_goods_id=row.id
+            console.log(row.repo)
+            let fd = new FormData()
+            fd.append('repo', row.repo)
+            fd.append('good_id', repo_goods_id)
+            postForm(`http://43.143.179.158:8080/updateRepo`, fd).then(res => {
+                this.tableData = res.goods
+            })
+                .catch(function (error) {
+                });
+            if (this.tableData == null) {
+                return false
+            }
+        },
+        goodsStatus(status) {
+            if (status == "1")
+                return "danger";
+            else if (status == "2")
+                return "";
+        },
+        changeNumPri(which) {
+            const length = this.tableData.length;
+            //总价
+            //全部数量
+            let allPricess = 0.0;
+            let allNumss = 0;
+            for (let i = 0; i < length; i++) {
+                if (this.tableData[i].status != 1) {
+                    allPricess += this.tableData[i].nums * this.tableData[i].price;
+                    allNumss += this.tableData[i].nums;
+                }
+            }
+            if (which == '2') {
+                return parseFloat(allPricess).toFixed(2);
+            } else {
+                return allNumss;
+            }
+        },
+        // 多选操作
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
+        },
+        getSellData() {
+            let fd = new FormData()
+            fd.append('user_id', localStorage.getItem('userId'))
+            postForm(`http://43.143.179.158:8080/getSellGoods`, fd).then(res => {
+                this.tableData = res.goods
+            })
+                .catch(function (error) {
+                });
+            if (this.tableData == null) {
+                return false
+            }
+            return true
+        }
     }
 }
 </script>
@@ -103,5 +237,70 @@ export default {
     height: 100px;
     line-height: 100px;
     margin-left: 60px;
+}
+
+  .new_goods {
+      position: absolute;
+      top: 20%;
+      left: 1%;
+      width: 75%;
+      height: 75%;
+      /*background-color: #409EFF;*/
+      /*text-align: center; */
+      /* vertical-align: middle; */
+  }
+
+
+.title {
+    margin-top: 50px;
+}
+
+.title h3 {
+    /*float: left;*/
+    font-size: 23px;
+    color: #c91623;
+    margin-bottom: 30px;
+}
+
+/*商品结算*/
+.balance {
+    height: 50px;
+    border: 1px solid gray;
+    margin-top: 40px;
+}
+
+.balance li {
+    float: left;
+    line-height: 5px;
+    margin-left: 22px;
+}
+
+.balance_ul2 {
+    float: right;
+    margin-right: 10px;
+}
+
+.balance_ul1 .button1 {
+    margin-top: 5px;
+}
+
+.balance_ul2 .spanText {
+    font-size: 25px;
+    color: #C91623;
+    font-weight: bold;
+}
+
+.balance_ul2 button {
+    width: 100px;
+    height: 50px;
+    background-color: brown;
+    border: 1px solid #c91623;
+    font-weight: bold;
+    font-size: 20px;
+    color: white;
+}
+
+.balance_ul2 button:hover {
+    background-color: #C91623;
 }
 </style>
