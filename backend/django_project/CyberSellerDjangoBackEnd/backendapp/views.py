fbd7d982a618ccd5ec87344935c38fbf0c53f656
YJK <yjk_official@foxmail.com>
2022-12-21 16:46:16 +0800

    fix: 尝试修复addGoodDetail  API

--- backend/django_project/CyberSellerDjangoBackEnd/backendapp/views.py
+++ backend/django_project/CyberSellerDjangoBackEnd/backendapp/views.py
@@ -929,8 +929,8 @@ def getGoodDetail(request):
 		good_details = GoodDetail.objects.filter(good_id=good_id)
 		ret_list = []
 		for good_detail in good_details:
-			key = good_detail['key']
-			value = good_detail['value']
+			key = good_detail.key
+			value = good_detail.value
 			ret_list.append({
 				'key': key,
 				'value': value
